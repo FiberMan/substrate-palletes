ecbe5b058f79cffc1091a24c3f27dfde657ffd48
Yurii Filimonchuk <filimonchuk@gmail.com>
2022-07-23 22:39:51 +0200

    Add tests to Reputation pallet.

--- reputation/src/lib.rs
+++ reputation/src/lib.rs
@@ -128,3 +128,109 @@ pub mod pallet {
 
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	// Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+	use crate as pallet_reputation;
+	use frame_support::{
+		assert_noop, assert_ok,
+		traits::{ConstU32, ConstU64},
+	};
+	use sp_core::H256;
+	use sp_runtime::{
+		testing::Header,
+		traits::{BlakeTwo256, IdentityLookup},
+	};
+
+	type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
+	type Block = frame_system::mocking::MockBlock<Test>;
+
+	frame_support::construct_runtime!(
+		pub enum Test where
+			Block = Block,
+			NodeBlock = Block,
+			UncheckedExtrinsic = UncheckedExtrinsic,
+		{
+			System: frame_system,
+			Reputation: pallet_reputation,
+		}
+	);
+	   
+	impl frame_system::Config for Test {
+		type BaseCallFilter = frame_support::traits::Everything;
+		type BlockWeights = ();
+		type BlockLength = ();
+		type DbWeight = ();
+		type Origin = Origin;
+		type Index = u64;
+		type BlockNumber = u64;
+		type Hash = H256;
+		type Call = Call;
+		type Hashing = BlakeTwo256;
+		type AccountId = u64;
+		type Lookup = IdentityLookup<Self::AccountId>;
+		type Header = Header;
+		type Event = Event;
+		type BlockHashCount = ConstU64<250>;
+		type Version = ();
+		type PalletInfo = PalletInfo;
+		type AccountData = ();
+		type OnNewAccount = ();
+		type OnKilledAccount = ();
+		type SystemWeightInfo = ();
+		type SS58Prefix = ();
+		type OnSetCode = ();
+		type MaxConsumers = ConstU32<16>;
+	}
+
+	impl Config for Test {
+		type MaxReputationChange = ConstU32<10>;
+		type Event = Event;
+	}
+
+	#[derive(Default)]
+	pub struct ExtBuilder;
+	impl ExtBuilder {
+		pub fn build(self) -> sp_io::TestExternalities {
+			let t = frame_system::GenesisConfig::default().build_storage::<Test>().unwrap();
+			let mut ext = sp_io::TestExternalities::new(t);
+			ext.execute_with(|| System::set_block_number(1));
+			ext
+		}
+	}
+	   
+	#[test]
+	fn should_increase_reputation() {
+		ExtBuilder::default().build().execute_with(|| {
+			assert_eq!(Reputations::<Test>::get(2), None);
+			assert_ok!(Reputation::increase_reputation(Origin::signed(1), 2, 5));
+			assert_eq!(Reputations::<Test>::get(2).unwrap(), 5);
+			assert_ok!(Reputation::increase_reputation(Origin::signed(1), 2, 7));
+			assert_eq!(Reputations::<Test>::get(2).unwrap(), 12);
+		});
+	}
+
+	#[test]
+	fn should_decrease_reputation() {
+		ExtBuilder::default().build().execute_with(|| {
+			assert_eq!(Reputations::<Test>::get(2), None);
+			assert_ok!(Reputation::increase_reputation(Origin::signed(1), 2, 8));
+			assert_eq!(Reputations::<Test>::get(2).unwrap(), 8);
+			assert_ok!(Reputation::decrease_reputation(Origin::signed(1), 2, 7));
+			assert_eq!(Reputations::<Test>::get(2).unwrap(), 1);
+			assert_ok!(Reputation::decrease_reputation(Origin::signed(1), 2, 7));
+			assert_eq!(Reputations::<Test>::get(2).unwrap(), 0);
+		});
+	}
+
+	#[test]
+	fn should_catch_errors() {
+		ExtBuilder::default().build().execute_with(|| {
+			assert_noop!(Reputation::increase_reputation(Origin::signed(1), 2, 0), Error::<Test>::ReputationTooLow);
+			assert_noop!(Reputation::increase_reputation(Origin::signed(1), 2, 20), Error::<Test>::ReputationTooHigh);
+			assert_noop!(Reputation::decrease_reputation(Origin::signed(1), 2, 5), Error::<Test>::NoReputationToDecrease);
+		});
+	}
+}
